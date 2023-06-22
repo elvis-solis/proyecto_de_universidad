280cf16789d4a2250549f917909798352012987d
Elvis <arnoldocare@gmail.com>
2023-06-22 09:59:33 -0600

    first commit

---
+++ controllers/dbconn.php
@@ -0,0 +1,11 @@
+<?php
+
+$host = "localhost";
+$pass = "";
+
+// $host = "localhost";
+// $pass = "nUkp-q[M";
+
+$user = "root";
+$database = "uni";
+$db = new mysqli($host, $user, $pass, $database);
